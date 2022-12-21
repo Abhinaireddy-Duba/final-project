0606228755c54fb7b4f5d8af15c583fd1c4b5a77
PNKishore01 <nagakishore3639@gamil.com>
2022-12-21 10:26:50 +0530

    Final Project

---
+++ Db/db.js
@@ -0,0 +1,20 @@
+const mongoose = require("mongoose");
+
+function connectDb() {
+  mongoose.connect("mongodb+srv://Abhinaireddy-Duba:Abhinai@cluster0.pobpvvm.mongodb.net/CarRentaldb?retryWrites=true&w=majority",
+   {
+    
+    useUnifiedTopology: true,
+    useNewUrlParser: true,
+  });
+  const connection = mongoose.connection;
+  connection.on("connected", () => {
+    console.log("connection successfull");
+  });
+  connection.on("error", () => {
+    console.log("connection failed");
+  });
+}
+
+connectDb();
+module.exports = mongoose;
